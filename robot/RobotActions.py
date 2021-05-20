ba375aed8367d9faa6755cb8e54b8d92f6f658d9
ThemisTek <59509230+ThemisTek@users.noreply.github.com>
2021-05-20 19:46:25 +0300

    Update Turning

--- robot/RobotActions.py
+++ robot/RobotActions.py
@@ -1,7 +1,7 @@
 from enum import Enum
 import picar
 from picar import front_wheels,back_wheels
-
+import time
 
 class RobotState(Enum):
     Initial = 0
@@ -24,25 +24,40 @@ class RobotRunner():
         picar.setup()
         self.bw = back_wheels.Back_Wheels()
         self.fw = front_wheels.Front_Wheels()
+        self.PreviousState = RobotState.Initial
         self.State = RobotState.Initial
         self.NNState = NeuralNetWorkRead.Unknown
         self.distance = 0
         self.confidence = 0
         self.confidenceNeed = 1
+        self.lastTime = time.time()
+        self.curTime = time.time()
+        self.timeDif = 0
+        self.distanceToTurn = 35
 
     def Update(self,NNState,distance,confidence):
         self.NNState = NNState
         self.distance = distance
         self.confidence = confidence
+
+        self.timeDif = self.curTime - self.lastTime
+    
+    def countTimeInState(self,changedState : bool):
+        self.curTime = time.time()
+        if(changedState):
+            self.lastTime = self.curTime
+        self.timeDif = self.lastTime - self.curTime
+
     
     def PrintState(self):
-        print(f'State : {self.State} NN : {self.NNState} conf : {self.confidence} dist : {self.distance}')
+        print(f'State : {self.State} NN : {self.NNState} conf : {self.confidence} dist : {self.distance} timeElapsed : {self.timeDif}')
     
     def Conf(self):
         return self.confidence >= self.confidenceNeed
 
     def UpdateState(self):
         self.PrintState()
+        self.PreviousState = self.State
         if(self.State == RobotState.Initial or self.State == RobotState.Stop):
             if(self.NNState == NeuralNetWorkRead.Up and self.Conf()):
                 self.State = RobotState.MovingForward
@@ -68,24 +83,25 @@ class RobotRunner():
         elif(self.State == RobotState.TurningRight or self.State == RobotState.TurningLeft):
             if(self.NNState == NeuralNetWorkRead.Stop and self.Conf()):
                 self.State = RobotState.Stop
-            elif(self.distance > 45):
+            elif(self.timeDif > 5):
                 self.State = RobotState.MovingForward
                 print('changed to')
                 self.PrintState()
         elif(self.State == RobotState.PrepartedToTurnLeft):
             if(self.NNState == NeuralNetWorkRead.Right and self.Conf()) :
                 self.State = RobotState.PreparedToTurnRight
-            elif(self.distance < 25):
+            elif(self.distance < self.distanceToTurn):
                 self.State = RobotState.TurningLeft
                 print('changed to')
                 self.PrintState()
         elif(self.State == RobotState.PreparedToTurnRight):
             if(self.NNState == NeuralNetWorkRead.Left and self.Conf()) :
                 self.State = RobotState.PreparedToTurnRight
-            elif(self.distance < 25):
+            elif(self.distance < self.distanceToTurn):
                 self.State = RobotState.TurningRight
                 print('changed to')
                 self.PrintState()
+        self.countTimeInState(self.PreviousState != self.State)
 
     def RunState (self):
         if(self.State == RobotState.Initial or self.State == RobotState.Stop):
