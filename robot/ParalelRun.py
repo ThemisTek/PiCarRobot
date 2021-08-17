1c11d9c7573a17698ebf0d44b8b1cc3749b57cfd
ThemisTek <59509230+ThemisTek@users.noreply.github.com>
2021-08-17 18:10:46 +0300

    ParalelTest

--- robot/ParalelRun.py
+++ robot/ParalelRun.py
@@ -1,7 +1,8 @@
+from logging import exception
 from multiprocessing import queues
 from multiprocessing.context import Process
-# from robot.RobotActions import RobotState
-# import RobotActions as RobotActions
+from robot.RobotActions import NeuralNetWorkRead, RobotState
+import RobotActions as RobotActions
 import numpy as np
 import cv2
 from keras.preprocessing import image
@@ -9,65 +10,86 @@ import time
 import os
 import multiprocessing as mp
 from enum import Enum
+import RPi.GPIO as GPIO
 
-class RobotState(Enum):
-    Initial = 0
-    MovingForward = 1
-    TurningRight = 2
-    TurningLeft = 3
-    Stop = 4
-    PreparedToTurnRight = 5
-    PrepartedToTurnLeft = 6
+GPIO_TRIGGER = 16
+GPIO_ECHO = 20
 
-class Test() : 
-    def __init__(self):
-        self.State = RobotState.Initial
-        self.dif = 0
+def distance():
+    GPIO.output(GPIO_TRIGGER, True)
+ 
+    # set Trigger after 0.01ms to LOW
+    time.sleep(0.00005)
+    GPIO.output(GPIO_TRIGGER, False)
+ 
+    StartTime = time.time()
+    StopTime = time.time()
+ 
+    # save StartTime
+    while GPIO.input(GPIO_ECHO) == 0:
+        StartTime = time.time()
+        if(StartTime-StopTime > 3):
+            break
+ 
+    # save time of arrival
+    while GPIO.input(GPIO_ECHO) == 1:
+        StopTime = time.time()
+        if(StopTime - StartTime > 3):
+            break        
+
+    TimeElapsed = StopTime - StartTime
+    distance = (TimeElapsed * 34300) / 2
+    GPIO.output(GPIO_TRIGGER, False)
+    return distance
+
+def RobotProccess(m):
+    RobotController = RobotActions.RobotRunner(LogInfo = False)
+    RobotController.RunState()
+    while True :
+        time.sleep(0.1)
+        NNState = m['NeuralNetworkState']
+        distance = m['distance']
+        confidence = 1
+        try :
+            RobotController.Update(NNState,distance,confidence,None)
+            RobotController.UpdateState()
+            RobotController.RunState()
+        except :
+            print("Robot Error")
+
+def DistanceProccess(m):
+    while True:
+        time.sleep(0.25)
+        try:
+            readDistance = distance()
+            m['distance'] = readDistance
+        except :
+            print("Distance Error") 
 
 
-    def SetState(self,newState):
-        self.State = newState    
-    def print(self):
-        print(self.State)
-    def Start(self):
-        self.p1.start()
-        self.p2.start()
-        self.p1.join()
-        self.p2.join()
 
-def GetTime(t,m):
-    print("startGetTime")
+
+def GetNeuralNetworkResponseProccess(m):
     start = time.time()
+
     while True:
         end = time.time()
         time.sleep(0.5)
-        t.value = end-start
-        state = m['State']
-        print(state)
 
-
-def OutPutAction(t,m):
-    print("StartOutPut")
-
-    while True:
-        time.sleep(0.05)
-        if(t.value<1):
-            m['State'] = RobotState.MovingForward
-            print("Less 1")
-        elif(t.value<3):
-            m['State'] = RobotState.TurningRight
-            print("Less 2")
-        elif(t.value<10):
-            m['State'] = RobotState.TurningLeft
 
 if __name__ == '__main__':
     m = mp.Manager().dict()
     t = mp.Value('d',0)
-    m['State'] = RobotState.Initial
+    m['NeuralNetworkState'] = NeuralNetWorkRead.Stop
+    m['distance'] = 0
+    m['confidence'] = 1
 
-    p1 = Process(target=GetTime,args=(t,m))
-    p2 = Process(target=OutPutAction,args=(t,m))
+    p1 = Process(target=RobotProccess,args=(m,))
+    p2 = Process(target=DistanceProccess,args=(m,))
+    p3 = Process(target=GetNeuralNetworkResponseProccess,args=(m,))
     p1.start()
     p2.start()
+    p3.start()
     p1.join()
     p2.join()
+    p3.join()
